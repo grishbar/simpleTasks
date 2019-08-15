2722711fe9cb13a5a2ca29709c779b2a96aa3c8b
grishbar <grishbar@mail.ru>
2019-08-15 16:28:19 +0500

    small linting fixes

--- task1/index.js
+++ task1/index.js
@@ -2,7 +2,7 @@ function main() {
     const input = require("fs").readFileSync("input.txt", "utf8");
     // допустим, что ввод всегда правильный. 
     let operation = (input.match(/[+?-?*?/?]/)).toString();
-    let tableSize = Number(input.slice(input.search(/[0-9]/),input.length));
+    let tableSize = Number(input.slice(input.search(/[0-9]/), input.length));
     fs = require("fs").writeFileSync("output.txt", getOutput(operation, tableSize), "utf8")
 }
 
@@ -19,26 +19,27 @@ function getResultArray(operation, tableSize) {
     for (let i = 1; i < tableSize + 2; i++) {
         for (let j = 1; j < tableSize + 2; j++) {
             resultArr[j][i] = (eval(resultArr[j][0] + operation + resultArr[0][i]));
+            //проверяем что число не целое, и что у него больше одного знака после запятой
             ((resultArr[j][i] ^ 0) !== resultArr[j][i] && resultArr[j][i].toString().length !== 3) ?
-            resultArr[j][i] = resultArr[j][i].toFixed(2): resultArr[j][i];
+            resultArr[j][i] = resultArr[j][i].toFixed(2) : resultArr[j][i];
         }
     }
-    console.log(resultArr);
+
     return resultArr;
-    
 }
 
 function getOutput(operation, tableSize) {
     let resultArr = getResultArray(operation, tableSize);
     let resultOutput = '';
     let maxColumnLength = 0;
-    for (let i = 0; i < resultArr[0].length - 1; i++) { // не берём последний столбец так как его менять не надо
+    let arrLength = resultArr[0].length;
+    for (let i = 0; i < arrLength - 1; i++) { // не берём последний столбец так как его менять не надо
         maxColumnLength = 0;
-        for (let j = 0; j < resultArr[0].length; j++) {
+        for (let j = 0; j < arrLength; j++) {
             (resultArr[j][i].toString().length > maxColumnLength) ?
-            maxColumnLength = resultArr[j][i].toString().length: maxColumnLength;
+            maxColumnLength = resultArr[j][i].toString().length : maxColumnLength;
         }
-        for (let j = 0; j < resultArr[0].length; j++) {
+        for (let j = 0; j < arrLength; j++) {
             let spaces = '';
             let k = resultArr[j][i].toString().length;
             for (; k <= maxColumnLength; k++) {
@@ -47,10 +48,11 @@ function getOutput(operation, tableSize) {
             resultArr[j][i] += spaces + '| ';
         }
     }
-    for (let i = 0; i < resultArr[0].length; i++) {
+    for (let i = 0; i < arrLength; i++) {
         resultOutput += resultArr[i].join('') + '\n';
         if (i === 0) {
-            let k =resultOutput.length - resultArr[0][resultArr[0].length - 1].toString().length +  maxColumnLength;  // длинна всей первой строки + максимальна длинна последнего столбца
+            // k - длинна всей первой строки + максимальна длинна последнего столбца
+            let k = resultOutput.length - resultArr[0][arrLength - 1].toString().length + maxColumnLength; 
             for (let j = 0; j < k; j++) {
                 resultOutput += '-';
             }
