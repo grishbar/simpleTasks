86112127c36383c93e83c9d0e3354afa097f348b
grishbar <grishbar@mail.ru>
2019-08-19 11:55:03 +0500

    delete unecessary lines index.js task3

--- task3/index.js
+++ task3/index.js
@@ -1,8 +1,8 @@
 function moveArrow(inputDegree) {
     let arrowContainer = document.getElementsByClassName("arrow-container")[0];
     arrowContainer.style.transform = "rotate(" + inputDegree + "deg)";
-    //secElt.style.webkitTransform = "rotate(" + turnSec + "deg)";
 }
+
 function changeDegrees() {
     let degreeNumber = document.getElementById("degree-number");
     let inputDegree = document.getElementById("set-degree").value;
@@ -13,14 +13,14 @@ function changeDegrees() {
 //так как наш контрол находится в диапозоне от 90 до 270 градусов 
 // а вводимые значения должны колебатся от 0 до 500, то
 function getRealDegree(inputDegree) {
-    Math.ceil(inputDegree * (500/180))
     if (inputDegree < 0) {
         return 90;
-    } 
+    }
 
-    return inputDegree > 500 ? 270 : 90 + Math.ceil(inputDegree * (180/500));
+    return inputDegree > 500 ? 270 : 90 + Math.ceil(inputDegree * (180 / 500));
 }
 
+/* //вначале отрисовать страницу, создать все элемнты только потом запускать скрипт 
 function ready(callback) {
     // in case the document is already rendered
     if (document.readyState != 'loading') callback();
@@ -34,4 +34,4 @@ function ready(callback) {
 
 ready(function () {
     moveArrow();
-});
+}); */
